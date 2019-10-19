06f2215764d499167bc58d1b7992e31580aa74b0
Yohan Dunon <yohandunon@icloud.com>
2019-10-19 10:50:58 +0200

    First commit

---
+++ battlegame/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  battlegame
+//
+//  Created by Yohan W. Dunon on 19/10/2019.
+//  Copyright © 2019 Yohan William Dunon. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
